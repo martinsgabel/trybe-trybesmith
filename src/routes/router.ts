102e88e4b76a63b4a60e1be8677792cf6b3ee46b
martins <gabrielamspinola@gmail.com>
2022-10-13 17:31:11 -0300

    correção: acréscimo de rauter para endpoint Users

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import ProductsC from '../controller/Products.controller';
 import OrdersC from '../controller/OrdersC.controller';
+import UsersC from '../controller/UsersC.controller';
 
 const routers = Router();
 const productsC = new ProductsC();
 const ordersC = new OrdersC();
+const usersC = new UsersC();
 
 routers.get('/products', productsC.AllProducts);
 routers.post('/products', productsC.AddProducts);
 routers.get('/orders', ordersC.AllOrders);
+routers.post('/users', usersC.newUser);
+routers.post('/login', usersC.loginUser);
 
 export default routers;
